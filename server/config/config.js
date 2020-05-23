bc8d315a4fbf5908ecacf0ca08c7397bb250f176
Marcelo Aiello <marcelo.aiello@gmail.com>
2020-05-23 13:07:35 -0300

    cambio URI para heroku

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://cafe-user:e0N5bhHf3B97lEea@cluster0-cwfiy.mongodb.net/cafe'
+    //urlDB = 'mongodb+srv://cafe-user:e0N5bhHf3B97lEea@cluster0-cwfiy.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 };
 
 process.env.URLDB = urlDB;
